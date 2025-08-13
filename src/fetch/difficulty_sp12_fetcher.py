167bba1d6a95cbe91ac4f48ccc281c3a50b0b7b3
chinimuruhi <31010801+chinimuruhi@users.noreply.github.com>
2025-08-13 17:24:57 +0900

    [FIx]難易度のfetch方式を変更

--- src/fetch/difficulty_sp12_fetcher.py
+++ src/fetch/difficulty_sp12_fetcher.py
@@ -18,6 +18,27 @@ class difficulty_sp12_data:
         'songs':'https://iidx-sp12.github.io/songs.json'
     }
 
+    
+    _TABLE_DIFFICULTY = {
+        'S+': 10,
+        'S': 9,
+        'A+': 8,
+        'A': 7,
+        'B+': 6,
+        'B': 5,
+        'C': 4,
+        'D': 3,
+        'E': 2,
+        'F': 1,
+        '未定': -1,
+        '不明': -2
+    }
+    
+    _SUB_DIFFICULTY = [
+        [0, '地力'],
+        [0.5, '個人差']
+    ]
+
     # コンストラクタ
     def __init__(self, logging):
         # Loggingオブジェクトの引き継ぎ
@@ -33,36 +54,45 @@ class difficulty_sp12_data:
             lst = []
             dct = {}
             difficulty = {
-                'normal':{
-                    '-1': '未定',
-                },
-                'hard':{
-                    '-1': '未定',
-                }
+                'normal': {},
+                'hard': {}
             }
+            difficulty_rev = {
+                'normal': {},
+                'hard': {}
+            }
+            for diff_key in self._TABLE_DIFFICULTY:
+                diff_value = self._TABLE_DIFFICULTY[diff_key]
+                if diff_value >=0 and diff_value <= 11:
+                    for diff_sub in self._SUB_DIFFICULTY:
+                        diff_subvalue = diff_value - diff_sub[0]
+                        diff_subkey = diff_sub[1] + diff_key
+                        difficulty['normal'][str(diff_subvalue)] = diff_subkey
+                        difficulty['hard'][str(diff_subvalue)] = diff_subkey
+                        difficulty_rev['normal'][diff_subkey] = diff_subvalue
+                        difficulty_rev['hard'][diff_subkey] = diff_subvalue
+                else:
+                    difficulty['normal'][str(diff_value)] = diff_key
+                    difficulty['hard'][str(diff_value)] = diff_key
+                    difficulty_rev['normal'][diff_key] = diff_value
+                    difficulty_rev['hard'][diff_key] = diff_value
+
             for song in songs:
                 # IDを取得
                 id = textage_data.get_song_id(song['name'])
                 if id == -1:
                     self._logging.error('[sp12]:'+ song['name'])
                 id_str = str(id)
-                n_value = int(song['n_value'])
-                h_value = int(song['h_value'])
-                if n_value == 0:
+                normal = song['normal']
+                hard = song['hard']
+                if normal and normal in difficulty_rev['normal']:
+                    n_value = difficulty_rev['normal'][normal]
+                else:
                     n_value = -1
-                if h_value == 0:
+                if hard and hard in difficulty_rev['hard']:
+                    h_value = difficulty_rev['hard'][hard]
+                else:
                     h_value = -1
-                # 惑星鉄道のデータのエラーを回避
-                if song['name'] == '惑星鉄道':
-                    n_value = 10
-                    h_value = 10
-                else :
-                    # ノマゲ難易度の文字の取得
-                    if n_value >= 0:
-                        difficulty['normal'][str(n_value)] = song['normal']
-                    # ハード難易度の文字の取得
-                    if h_value >= 0:
-                        difficulty['hard'][str(h_value)] = song['hard']
                 # データの成形
                 lst.append({
                     'id': id,
